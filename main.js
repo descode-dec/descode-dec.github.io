c07188d5a9b5907fbee9902e17404ddc25fd87cc
descode-dec <53048233+descode-dec@users.noreply.github.com>
2020-01-01 13:11:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -165,7 +165,7 @@ Search.prototype = {
 				this.oLis[i].className = 'iconfont';
 			}
 			ele.className = 'iconfont using';
-			setCookie('defaultTools', tools, 31);
+			setCookie('defaultTool', tools, 31);
 			this.searchTool = tools;
 			this.oToolsList.style.boxShadow = '';
 			this.flag1 = false;
@@ -236,4 +236,4 @@ var baidu = {
 		searchObj.keydata(key);
 	}
 }
-console.log("%cda i su ki Sakurajima Mai --2019/12/31 21：46：42", "background-image: linear-gradient(to right, red, blue);color: #fff");
+console.log("%cda i su ki Sakurajima Mai --2019/12/31", "background-image: linear-gradient(to right, red, blue);color: #fff");
